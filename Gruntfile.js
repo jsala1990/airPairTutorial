81214b7c3ce63aeedd0de8a374e61fabe51e91fe
Jesus <jsalazar@amplify.com>
2015-03-14 21:54:04 -0400

    http server

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,13 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        port: 8000
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-connect');
   grunt.registerTask('default', function() {
     console.log('started');
   });
